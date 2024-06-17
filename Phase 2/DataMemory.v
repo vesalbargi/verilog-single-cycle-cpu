8dbab83d3533e634469f3cd2dc88c816e3f3a5d3
Vesal Bargi <vesalbargi81@gmail.com>
2024-06-17 12:23:43 +0330

    refactor: format code

--- Phase 2/DataMemory.v
+++ Phase 2/DataMemory.v
@@ -14,13 +14,12 @@ module DataMemory (
   integer i;
   always @(posedge clk) begin
     if (Startin) begin
-      for (i = 0; i < 41; i = i + 4)
-      begin
-        {memory[i + 3], memory[i + 2], memory[i + 1], memory[i]} <= 1;
+      for (i = 0; i < 41; i = i + 4) begin
+        {memory[i+3], memory[i+2], memory[i+1], memory[i]} <= 1;
       end
     end
     if (MemWrite) begin
-      {memory[Address + 3], memory[Address + 2], memory[Address + 1], memory[Address]} <= WriteData;
+      {memory[Address+3], memory[Address+2], memory[Address+1], memory[Address]} <= WriteData;
     end
   end
 endmodule
